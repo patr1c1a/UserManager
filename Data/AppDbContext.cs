16ba9f2fb5701cbcdf39e999af2f4b82c7a9b84b
Patricia E. Miguel <contacto@patriciaemiguel.com>
2025-01-25 19:37:35 -0300

    Cleaned up Program.cs and removed default example files

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace UserManager.Data
+{
+	public class AppDbContext : DbContext
+	{
+    	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    	// Tables:
+    	public DbSet<User> Users { get; set; }
+    	public DbSet<Role> Roles { get; set; }
+	}
+}
