bac3a6c9bee520685ec0e287949371ee35350e52
Patricia E. Miguel <contacto@patriciaemiguel.com>
2025-01-26 16:49:51 -0300

    Added validation annotations and Swagger schema descriptions for User model.

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using Swashbuckle.AspNetCore.Annotations;
+
 namespace UserManager.Models
 {
     public class User
     {
+        [SwaggerSchema(ReadOnly = true, Description = "Unique identifier for the User.")]
         public int Id { get; set; }
+        
+        [Required(ErrorMessage = "The username is required.")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "The username must be between {2} and {1} characters.")]
+        [SwaggerSchema(Description = "The unique username of the user.")]
         public required string Username { get; set; }
+
+        [EmailAddress(ErrorMessage = "The email is not valid.")]
+        [SwaggerSchema(Description = "The email address of the user.")]
         public string? Email { get; set; }
+
+        [Required(ErrorMessage = "The password is required.")]
+        [SwaggerSchema(Description = "The password hash of the user.")]
         public required string PasswordHash { get; set; }
-        public int RoleId { get; set; }
+
+        [Required]
+        [SwaggerSchema(Description = "The role ID associated with the user.")]
+        public required int RoleId { get; set; }
+
+        [SwaggerSchema(Description = "The role associated with the user.")]
         public required Role Role { get; set; }
     }
 }
