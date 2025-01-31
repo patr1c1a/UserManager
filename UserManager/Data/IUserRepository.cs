febb68b2be7ee6f8e6b7654f196a70182212b2e9
Patricia E. Miguel <contacto@patriciaemiguel.com>
2025-01-30 22:16:29 -0300

    Added interface to abstract User database operations.

---
+++ UserManager/Data/IUserRepository.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UserManager.Models;
+
+namespace UserManager.Data
+{
+    public interface IUserRepository
+    {
+        List<User> GetAllUsers();
+        User GetUserById(int id);
+        void AddUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(int id);
+    }
+}
---
+++ UserManager/Data/UserRepository.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+using UserManager.Models;
+
+namespace UserManager.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        public User GetUserById(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        public void AddUser(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        public void UpdateUser(User user)
+        {
+            _context.Users.Update(user);
+            _context.SaveChanges();
+        }
+
+        public void DeleteUser(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
