211a0492bd640ecc354365f2f186584f0a688c22
Patricia E. Miguel <contacto@patriciaemiguel.com>
2025-02-03 09:57:50 -0300

    Added ValidateUser method to IUserRepository.

--- UserManager/Data/IUserRepository.cs
+++ UserManager/Data/IUserRepository.cs
@@ -9,6 +9,7 @@ namespace UserManager.Data
         User? GetUserById(int id);
         User? GetUserByUsername(string name);
         void AddUser(User user);
+        bool ValidateUser(string username, string password);
         void UpdateUser(User user);
         void DeleteUser(int id);
     }
